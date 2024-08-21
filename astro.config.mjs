e922e679f63d7542e42b87847a2650fbf48147f5
Gabriel <gabi0526287131@gmail.com>
2024-08-21 15:46:04 +0300

    fixed site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://github.com/TeaCup404',
+  site: 'https://teacup404.github.io',
   base: 'FlashServer-Docs',
   integrations: [starlight({
     title: 'FlashServer',
